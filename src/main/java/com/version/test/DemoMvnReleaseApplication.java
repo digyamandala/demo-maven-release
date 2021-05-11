809363a09fa9b22c466c595b5215e2f8791af11d
Digya Mandala Putra <digyamp@gmail.com>
2021-05-12 01:30:17 +0700

    first commit

---
+++ src/main/java/com/version/test/DemoMvnReleaseApplication.java
@@ -0,0 +1,12 @@
+package com.version.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvnReleaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoMvnReleaseApplication.class, args);
+  }
+}
---
+++ src/test/java/com/version/test/DemoMvnReleaseApplicationTests.java
@@ -0,0 +1,12 @@
+package com.version.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvnReleaseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
